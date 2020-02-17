5ab601f0ef02cf495da7ab3099c806a67efd7cc6
Witold Rugowski <rugowski@nhw.pl>
2020-02-17 21:04:30 +0100

    more functions moved

--- src/main.cpp
+++ src/main.cpp
@@ -240,32 +240,6 @@ void create_basic_auth_strings() {
 	}
 }
 
-/*****************************************************************
- * html helper functions                                         *
- *****************************************************************/
-
-String make_header(const String& title) {
-	String s = FPSTR(WEB_PAGE_HEADER);
-	s.replace("{tt}", FPSTR(INTL_PM_SENSOR));
-	s.replace("{h}", FPSTR(INTL_HOME));
-	if(title != " ") {
-		s.replace("{n}", F("&raquo;"));
-	} else {
-		s.replace("{n}", "");
-	}
-	s.replace("{t}", title);
-	s.replace("{id}", esp_chipid());
-	s.replace("{mac}", WiFi.macAddress());
-	s.replace("{fwt}", FPSTR(INTL_FIRMWARE));
-	s.replace("{fw}", SOFTWARE_VERSION);
-	return s;
-}
-
-String make_footer() {
-	String s = FPSTR(WEB_PAGE_FOOTER);
-	s.replace("{t}", FPSTR(INTL_BACK_TO_HOME));
-	return s;
-}
 
 
 static String tmpl(const String& patt, const String& value) {
@@ -365,53 +339,6 @@ String add_sensor_type(const String& sensor_text) {
 	return s;
 }
 
-/*****************************************************************
- * Webserver request auth: prompt for BasicAuth
- *
- * -Provide BasicAuth for all page contexts except /values and images
- *****************************************************************/
-static bool webserver_request_auth() {
-	debug_out(F("validate request auth..."), DEBUG_MIN_INFO, 1);
-	if (cfg::www_basicauth_enabled && ! wificonfig_loop) {
-		if (!server.authenticate(cfg::www_username, cfg::www_password)) {
-			server.requestAuthentication();
-			return false;
-		}
-	}
-	return true;
-}
-
-
-/*****************************************************************
- * Webserver root: show all options                              *
- *****************************************************************/
-void webserver_root() {
-	if (WiFi.status() != WL_CONNECTED) {
-		sendHttpRedirect(server);
-	} else {
-		if (!webserver_request_auth())
-		{ return; }
-
-		String page_content = make_header(" ");
-		last_page_load = millis();
-		debug_out(F("output root page..."), DEBUG_MIN_INFO, 1);
-		page_content += FPSTR(WEB_ROOT_PAGE_CONTENT);
-		page_content.replace("{t}", FPSTR(INTL_CURRENT_DATA));
-		page_content.replace(F("{map}"), FPSTR(INTL_ACTIVE_SENSORS_MAP));
-		page_content.replace(F("{conf}"), FPSTR(INTL_CONFIGURATION));
-		page_content.replace(F("{conf_delete}"), FPSTR(INTL_CONFIGURATION_DELETE));
-		page_content.replace(F("{restart}"), FPSTR(INTL_RESTART_SENSOR));
-		page_content.replace(F("{debug_level}"), FPSTR(INTL_DEBUG_LEVEL));
-		page_content.replace(F("{none}"), FPSTR(INTL_NONE));
-		page_content.replace(F("{error}"), FPSTR(INTL_ERROR));
-		page_content.replace(F("{warning}"), FPSTR(INTL_WARNING));
-		page_content.replace(F("{min_info}"), FPSTR(INTL_MIN_INFO));
-		page_content.replace(F("{med_info}"), FPSTR(INTL_MED_INFO));
-		page_content.replace(F("{max_info}"), FPSTR(INTL_MAX_INFO));
-		page_content += make_footer();
-		server.send(200, FPSTR(TXT_CONTENT_TYPE_TEXT_HTML), page_content);
-	}
-}
 
 static int constexpr constexprstrlen(const char* str) {
 	return *str ? 1 + constexprstrlen(str + 1) : 0;
--- src/webserver.h
+++ src/webserver.h
@@ -20,5 +20,22 @@ void webserver_images();
  * Webserver page not found                                      *
  *****************************************************************/
 void webserver_not_found();
+/*****************************************************************
+ * Webserver request auth: prompt for BasicAuth
+ *
+ * -Provide BasicAuth for all page contexts except /values and images
+ *****************************************************************/
+bool webserver_request_auth();
 
+/*****************************************************************
+ * Webserver root: show all options                              *
+ *****************************************************************/
+void webserver_root();
+
+/*****************************************************************
+ * html helper functions                                         *
+ *****************************************************************/
+
+String make_header(const String& title);
+String make_footer();
 #endif //NAMF_WEBSERVER_H
