43d01770c1fed03f9782324b64e1b3a972b3fc4a
Witold Rugowski <rugowski@nhw.pl>
2020-02-22 16:09:40 +0100

    New BME280 library - changed behavior in our code to avoid hangs when sensors gets disconnected

--- src/sensors/bme280.cpp
+++ src/sensors/bme280.cpp
@@ -39,32 +39,35 @@ String sensorBME280() {
 
     debug_out(String(FPSTR(DBG_TXT_START_READING)) + FPSTR(SENSORS_BME280), DEBUG_MED_INFO, 1);
 
-    bme280.takeForcedMeasurement();
-    const auto t = bme280.readTemperature();
-    const auto h = bme280.readHumidity();
-    const auto p = bme280.readPressure();
-    if (isnan(t) || isnan(h) || isnan(p)) {
-        last_value_BME280_T = -128.0;
-        last_value_BME280_H = -1.0;
-        last_value_BME280_P = -1.0;
+    if (bme280.takeForcedMeasurement()) {
+        const auto t = bme280.readTemperature();
+        const auto h = bme280.readHumidity();
+        const auto p = bme280.readPressure();
+        if (isnan(t) || isnan(h) || isnan(p)) {
+            last_value_BME280_T = -128.0;
+            last_value_BME280_H = -1.0;
+            last_value_BME280_P = -1.0;
+            debug_out(String(FPSTR(SENSORS_BME280)) + FPSTR(DBG_TXT_COULDNT_BE_READ), DEBUG_ERROR, 1);
+            return s;
+        } else {
+            debug_out(FPSTR(DBG_TXT_TEMPERATURE), DEBUG_MIN_INFO, 0);
+            debug_out(Float2String(t) + " C", DEBUG_MIN_INFO, 1);
+            debug_out(FPSTR(DBG_TXT_HUMIDITY), DEBUG_MIN_INFO, 0);
+            debug_out(Float2String(h) + " %", DEBUG_MIN_INFO, 1);
+            debug_out(FPSTR(DBG_TXT_PRESSURE), DEBUG_MIN_INFO, 0);
+            debug_out(Float2String(p / 100) + " hPa", DEBUG_MIN_INFO, 1);
+            last_value_BME280_T = t;
+            last_value_BME280_H = h;
+            last_value_BME280_P = p;
+            s += Value2Json(F("BME280_temperature"), Float2String(last_value_BME280_T));
+            s += Value2Json(F("BME280_humidity"), Float2String(last_value_BME280_H));
+            s += Value2Json(F("BME280_pressure"), Float2String(last_value_BME280_P));
+        }
+        debug_out("----", DEBUG_MIN_INFO, 1);
+    } else {
         debug_out(String(FPSTR(SENSORS_BME280)) + FPSTR(DBG_TXT_COULDNT_BE_READ), DEBUG_ERROR, 1);
         return s;
-    } else {
-        debug_out(FPSTR(DBG_TXT_TEMPERATURE), DEBUG_MIN_INFO, 0);
-        debug_out(Float2String(t) + " C", DEBUG_MIN_INFO, 1);
-        debug_out(FPSTR(DBG_TXT_HUMIDITY), DEBUG_MIN_INFO, 0);
-        debug_out(Float2String(h) + " %", DEBUG_MIN_INFO, 1);
-        debug_out(FPSTR(DBG_TXT_PRESSURE), DEBUG_MIN_INFO, 0);
-        debug_out(Float2String(p / 100) + " hPa", DEBUG_MIN_INFO, 1);
-        last_value_BME280_T = t;
-        last_value_BME280_H = h;
-        last_value_BME280_P = p;
-        s += Value2Json(F("BME280_temperature"), Float2String(last_value_BME280_T));
-        s += Value2Json(F("BME280_humidity"), Float2String(last_value_BME280_H));
-        s += Value2Json(F("BME280_pressure"), Float2String(last_value_BME280_P));
     }
-    debug_out("----", DEBUG_MIN_INFO, 1);
-
     debug_out(String(FPSTR(DBG_TXT_END_READING)) + FPSTR(SENSORS_BME280), DEBUG_MED_INFO, 1);
 
     return s;
