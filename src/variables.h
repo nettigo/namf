1d98382e5e8acae05a9664da0a968f443f99ced4
Julian Szulc <github@julian.net.pl>
2020-10-03 13:07:34 +0200

    update version

--- src/variables.h
+++ src/variables.h
@@ -8,7 +8,7 @@
 #if defined(BOOT_FW)
 #define SOFTWARE_VERSION  "NAMF-2020-boot"
 #else
-#define SOFTWARE_VERSION  "NAMF-2020-32-MQTT"
+#define SOFTWARE_VERSION  "NAMF-2020-34rc3"
 #endif
 #include "defines.h"
 #include "system/scheduler.h"
