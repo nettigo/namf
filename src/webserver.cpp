e9ca0b701546a2b10335a9e8a3db7d37a1df6a70
netmaniac <rugowski@nhw.pl>
2021-03-23 11:16:02 +0100

    reformat

--- src/webserver.cpp
+++ src/webserver.cpp
@@ -837,6 +837,7 @@ void webserver_values() {
         }
 
         SimpleScheduler::getResultsAsHTML(page_content);
+
         page_content += FPSTR(EMPTY_ROW);
         page_content += table_row_from_value(F("NAM"),FPSTR(INTL_NUMBER_OF_MEASUREMENTS),String(count_sends),"");
         page_content += table_row_from_value(F("NAM"),F("Uptime"), millisToTime(millis()),"");
