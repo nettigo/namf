dc950de364d1d4a5e4ddf6916e70f1174a383c3c
netmaniac <rugowski@nhw.pl>
2021-04-06 21:32:48 +0200

    NAMF-2020-37a5

--- src/variables.h
+++ src/variables.h
@@ -8,7 +8,7 @@
 #if defined(BOOT_FW)
 #define SOFTWARE_VERSION  "NAMF-2020-boot"
 #else
-#define SOFTWARE_VERSION  "NAMF-2020-37a4"
+#define SOFTWARE_VERSION  "NAMF-2020-37a5"
 #endif
 #include "defines.h"
 #include "system/scheduler.h"
