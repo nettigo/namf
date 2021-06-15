633b2099ee787faad2779e5d1868cd880f284d1f
netmaniac <rugowski@nhw.pl>
2021-06-15 14:47:35 +0200

    call perform_work less frequent..

--- src/main.cpp
+++ src/main.cpp
@@ -849,7 +849,7 @@ void setup() {
         serialSDS.perform_work();
         serialGPS.perform_work();
         return true;
-    }, 1);
+    }, 100);
 //    serialSDS.enableIntTx(false);
 
     Wire.begin(I2C_PIN_SDA, I2C_PIN_SCL);
