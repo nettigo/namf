f1d001c4a7ea24bac74f17e12d9f47bc9cd4dcab
netmaniac <rugowski@nhw.pl>
2021-11-08 13:04:42 +0100

    NAMF-2020-41 release - version string updated

--- src/variables.h
+++ src/variables.h
@@ -8,7 +8,7 @@
 #if defined(BOOT_FW)
 #define SOFTWARE_VERSION  "NAMF-2020-boot"
 #else
-#define SOFTWARE_VERSION  "NAMF-2020-41rc4"
+#define SOFTWARE_VERSION  "NAMF-2020-41"
 #endif
 #include "defines.h"
 #include "system/scheduler.h"
