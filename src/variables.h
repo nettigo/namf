9a17a8a8c26ed5358e8825bae4fb7f51729f5219
netmaniac <rugowski@nhw.pl>
2021-11-08 13:28:22 +0100

    new beta

--- src/variables.h
+++ src/variables.h
@@ -8,7 +8,7 @@
 #if defined(BOOT_FW)
 #define SOFTWARE_VERSION  "NAMF-2020-boot"
 #else
-#define SOFTWARE_VERSION  "NAMF-2020-41"
+#define SOFTWARE_VERSION  "NAMF-2020-42rc1"
 #endif
 #include "defines.h"
 #include "system/scheduler.h"
