0c97c56e2f0182aefcdc42d9504ad0b6c6a12bad
netmaniac <rugowski@nhw.pl>
2021-11-25 14:52:58 +0100

    Fix for non existing language in config file (avoid loop at boot).

--- src/variables.h
+++ src/variables.h
@@ -8,7 +8,7 @@
 #if defined(BOOT_FW)
 #define SOFTWARE_VERSION  "NAMF-2020-boot"
 #else
-#define SOFTWARE_VERSION  "NAMF-2020-42rc1"
+#define SOFTWARE_VERSION  "NAMF-2020-42rc3"
 // undefine SOFTWARE_BETA in production releases
 #define SOFTWARE_BETA  1
 #endif
