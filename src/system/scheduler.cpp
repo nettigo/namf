e367581a3c170b5943fd0c2b644936c700113bd0
Witold Rugowski <rugowski@nhw.pl>
2024-03-22 16:47:47 +0100

    Start adding support to BH1750 - ambient light sensor

--- src/system/scheduler.cpp
+++ src/system/scheduler.cpp
@@ -17,10 +17,11 @@ namespace SimpleScheduler {
     const char LET_6 [] PROGMEM = "SPS30";
     const char LET_7 [] PROGMEM = "NTW_WTD";
     const char LET_8 [] PROGMEM = "MHZ14A";
+    const char LET_9 [] PROGMEM = "BH1750";
     const char LET_SIZE [] PROGMEM = "SIZE";
 
     const char *LET_NAMES[] PROGMEM = {
-            LET_0, LET_1, LET_2, LET_3, LET_4, LET_5, LET_6, LET_7, LET_8, LET_SIZE
+            LET_0, LET_1, LET_2, LET_3, LET_4, LET_5, LET_6, LET_7, LET_8, LET_9, LET_SIZE
     };
 
     unsigned long nullF(LoopEventType event) { return 0; }
