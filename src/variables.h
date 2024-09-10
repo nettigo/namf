b028bb217b91f130c220685189ece4c52989a5bb
Witold Rugowski <rugowski@nhw.pl>
2024-09-10 21:46:44 +0200

    NAMF-2020-46rc12

--- src/variables.h
+++ src/variables.h
@@ -9,8 +9,8 @@
 #define SOFTWARE_VERSION  "NAMF-2020-boot"
 #define SOFTWARE_VERSION_SHORT "boot"
 #else
-#define SOFTWARE_VERSION  "NAMF-2020-46rc11"
-#define SOFTWARE_VERSION_SHORT "46rc11"
+#define SOFTWARE_VERSION  "NAMF-2020-46rc12"
+#define SOFTWARE_VERSION_SHORT "46rc12"
 // undefine SOFTWARE_BETA in production releases
 #define SOFTWARE_BETA  1
 #endif
