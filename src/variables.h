343c355cff71cb60c39decb91baa920efd4589c3
Witold Rugowski <rugowski@nhw.pl>
2026-02-09 23:25:13 +0100

    next rc version

--- src/variables.h
+++ src/variables.h
@@ -9,8 +9,8 @@
 #define SOFTWARE_VERSION  "NAMF-2020-boot"
 #define SOFTWARE_VERSION_SHORT "boot"
 #else
-#define SOFTWARE_VERSION  "NAMF-48rc1"
-#define SOFTWARE_VERSION_SHORT "48rc1"
+#define SOFTWARE_VERSION  "NAMF-48rc2"
+#define SOFTWARE_VERSION_SHORT "48rc2"
 // undefine SOFTWARE_BETA in production releases
 #define SOFTWARE_BETA  1
 #endif
